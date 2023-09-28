175816c14cfa3d06425cbe027ab5e049c5a1a421
Alexandrsgit <alexandr.shevchenko.mail@gmail.com>
2023-09-28 16:59:36 +0500

    bugfix2

--- backend/recipes/management/commands/import_data.py
+++ backend/recipes/management/commands/import_data.py
@@ -30,18 +30,16 @@ class Command(BaseCommand):
     help = 'Импорт данных из CSV'
 
     def handle(self, *args, **options):
-        path = os.path.join(settings.BASE_DIR, 'ingredients.csv')
+        path = os.path.join(settings.BASE_DIR / 'data', 'ingredients.csv')
         with open(path, 'r', encoding='utf-8') as file:
             reader = csv.reader(file)
-            next(reader)
             ingredients = [ingredient_create(row) for row in reader]
             Ingredient.objects.bulk_create(ingredients)
         self.stdout.write(' - Данные успешно импортированны!')
 
-        path = os.path.join(settings.BASE_DIR, 'tags.csv')
+        path = os.path.join(settings.BASE_DIR / 'data', 'tags.csv')
         with open(path, 'r', encoding='utf-8') as file:
             reader = csv.reader(file)
-            next(reader)
             tags = [tag_create(row) for row in reader]
             Tag.objects.bulk_create(tags)
         self.stdout.write(' - Данные успешно импортированны!')
