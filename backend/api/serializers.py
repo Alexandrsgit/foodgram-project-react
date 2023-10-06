a356f66381ba298509ec77de8ba4e6ca2a8d6371
Alexandrsgit <alexandr.shevchenko.mail@gmail.com>
2023-10-06 17:19:49 +0500

    ater_3rd_review_06/10

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -306,14 +306,13 @@ class FavoriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Favorite
         fields = ('id', 'name', 'image', 'coocking_time')
-
-    def validate(self, data):
-        user = data['user']
-        if user.favorite.filter(recipe=data['recipe']).exists():
-            raise serializers.ValidationError(
-                'Рецепт уже добавлен в избранное.'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Favorite.objects.all(),
+                fields=('user', 'recipe'),
+                message='Рецепт уже добавлен в избранное'
             )
-        return data
+        ]
 
 
 class ShoppingCartSerializer(serializers.ModelSerializer):
@@ -335,11 +334,10 @@ class ShoppingCartSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShoppingCart
         fields = ('id', 'name', 'image', 'coocking_time')
-
-    def validate(self, data):
-        user = data['user']
-        if user.shopping_cart.filter(recipe=data['recipe']).exists():
-            raise serializers.ValidationError(
-                'Рецепт уже добавлен в корзину'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=ShoppingCart.objects.all(),
+                fields=('user', 'recipe'),
+                message='Рецепт уже добавлен в список покупок'
             )
-        return data
+        ]
